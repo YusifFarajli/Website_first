f150cd3454bdc35fb9201d6baa58707971a8fcfa
ardit <fyuskas@gmail.com>
2023-08-13 15:29:29 +0400

    Render the app titles in two columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 st.set_page_config(layout='wide')
 col1,col2 =st.columns(2)
 with col1 :
@@ -6,6 +7,21 @@ with col1 :
 with col2:
     st.title("Ardit Sulce")
     content="""
-    Hi,I am a Python programmer.I study at the UNEC(University of Economics).
+    "Lorem ipsum dolor sit amet,
+     consectetur adipiscing elit. Quisque quis facilisis velit. Nunc justo lorem, feugiat molestie faucibus ac, 
+     consequat nec magna. Praesent convallis tortor et tortor dapibus, a tincidunt felis finibus. Quisque purus nisl, 
+     malesuada id lacinia nec, sollicitudin ut eros. Mauris mollis mauris in felis viverra, eget pulvinar lorem porta. 
+     Sed id ullamcorper massa, eget tempus libero. Proin eu rhoncus mauris. Mauris ac pharetra turpis..
     """
     st.info(content)
+
+col3,col4=st.columns(2)
+df =pd.read_csv("data.csv",sep=";")
+
+with col3:
+    for index,row in df.iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])
